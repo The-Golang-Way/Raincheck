55621e7bc054b04d734d9616c2bc706a2d31301a
Yehdar <p.radhey06@gmail.com>
2023-06-16 16:26:21 -0400

    added weatherapi and error handler

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main(){
-	fmt.Println("hey honey! where's my super suit?")
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=8bd70fc3fa2e4593815201535231606&q=Toronto&aqi=no")
+	if err != nil{
+		panic(err)
+	}
 }
