30223628626a569dc4297e891f4dcf3bef10c4f1
Yehdar <p.radhey06@gmail.com>
2023-06-16 16:58:52 -0400

    'extrapolated' data after 'querying' through JSON

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+// this is giving me a headache man idek if im doing this right
+type Weather struct{
+	Location struct{
+		Name string `json:"name"`
+	} `json:"location"`
+	Current struct{
+		TempC float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json: "text"`
+		} `json: condition`
+	} `json:"current"`
+}
+
 func main(){
 	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=8bd70fc3fa2e4593815201535231606&q=Toronto&aqi=no")
 	if err != nil{
