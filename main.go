51eeb6670cb2dceb5b5c97578dfb2438915d1540
Yehdar <p.radhey06@gmail.com>
2023-06-17 11:25:22 -0400

    added user input for city

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 // this is giving me a headache man idek if im doing this right
@@ -23,7 +24,12 @@ type Weather struct{
 }
 
 func main(){
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=8bd70fc3fa2e4593815201535231606&q=Toronto&aqi=no")
+	userInput := "Toronto"
+	if len(os.Args) >= 2{
+		userInput = os.Args[1]
+	}
+
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=8bd70fc3fa2e4593815201535231606&q="+ userInput+"&aqi=no")
 	if err != nil{
 		panic(err)
 	}
